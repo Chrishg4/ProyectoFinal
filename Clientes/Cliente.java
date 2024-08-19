c77cbb409d89f78f15debc0362ebc8f5d1bd2ac9
Christofer <chhernandezga@est.utn.ac.cr>
2024-08-19 11:34:37 -0600

    Moodificacion de clase Clientes y Creacion de Clase lista Cliente
    se agregaron metodos y atributos

--- Clientes/Cliente.java
+++ Clientes/Cliente.java
@@ -4,10 +4,90 @@
  */
 package Clientes;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 /**
  *
  * @author chris
  */
 public class Cliente {
+    private int id;
+    private String nombre;
+    private LocalDate fechaNac;
+    private String telefono;
+    private String correo;
+    private int edad; 
+
+    public int getIdentificacion() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNac;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correo;
+    }
     
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setIdentificacion(int identificacion) {
+        this.id = identificacion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNac = fechaNacimiento;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correo = correoElectronico;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Cliente(int id, String nombre, LocalDate fechaNac, String telefono, String correo, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.edad = edad;
+    }
+
+    public Cliente() {
+        this(0,"",LocalDate.now(),"","",0);
+    }
+    
+    
+      
+    // Calcular edad del cliente
+    public int calcularEdad() {
+        LocalDate fechaActual = LocalDate.now();
+        Period edad = Period.between(getFechaNacimiento(), fechaActual);
+        return edad.getYears();
+    }
+   
 }
+
---
+++ ListaClientes/ListaCliente.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ListaClientes;
+
+import Clientes.Cliente;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author chris
+ */
+public class ListaCliente {
+     private ArrayList<Cliente> listaClientes = new ArrayList<>();
+
+    public ArrayList<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+     
+    public void agregarCliente(Cliente cliente) {
+        listaClientes.add(cliente);
+    }
+
+    public void actualizarCliente(int id, String telefono, String correo) {
+        for (Cliente cliente : listaClientes) {
+            if (cliente.getIdentificacion() == id) {
+                cliente.setTelefono(telefono);
+                cliente.setCorreoElectronico(correo);
+                break;
+            }
+        }
+    }
+
+    public void eliminarCliente(int id) throws Exception {
+        for (Cliente cliente : listaClientes) {
+            if (cliente.getIdentificacion() == id) {
+                if (tienePaquetesEnTransito(cliente)) {
+                    throw new Exception("No se puede eliminar el cliente porque tiene paquetes en tránsito");
+                }
+                listaClientes.remove(cliente);
+                break;
+            }
+        }
+    }
+
+    public Cliente buscarCliente(int id) {
+        for (Cliente cliente : listaClientes) {
+            if (cliente.getIdentificacion() == id) {
+                return cliente;
+            }
+        }
+        return null;
+    }
+
+    private boolean tienePaquetesEnTransito(Cliente cliente) {
+        // lista de paquetes
+        // ...
+        return false; // No tiene
+    }
+    
+}
+
